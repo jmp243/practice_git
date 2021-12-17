a6bb38a1683d961f948c4fba59e11c21cb48830a
Jung Mee Park <jpark1917@gmail.comecho>
2021-12-16 20:52:28 -0700

    practice

---
+++ practice_script.R
@@ -0,0 +1,8 @@
+fahrenheit_to_kelvin <- function(temp_F) {
+  temp_C <- fahrenheit_to_celsius(temp_F)
+  temp_K <- celsius_to_kelvin(temp_C)
+  return(temp_K)
+}
+
+# freezing point of water in Kelvin
+fahrenheit_to_kelvin(32.0)
